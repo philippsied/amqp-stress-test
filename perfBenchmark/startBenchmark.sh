e4aa132b395578d436c52c766ff077bec239efa0
blake <blake@blakesstube.congstar.box>
2015-05-11 16:15:08 +0200

    Add benchmark setup

---
+++ perfBenchmark/startBenchmark.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CLIENTDIR=rabbitmq-java-client-bin-3.5.1
+TESTENV=environment
+TEMPLATE=publish-consume-spec.template
+SPEC=publish-consume-spec.js
+TARGET=publish-consume-result.json
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <URI without amqp://>"
+    exit 0
+fi
+
+cd $TESTENV
+bash setupSpecs.sh $TEMPLATE $SPEC $1
+cd ..
+cd $CLIENTDIR
+sh runjava.sh com.rabbitmq.examples.PerfTestMulti ../$TESTENV/$SPEC ../$TESTENV/$TARGET
+cd ..
+
