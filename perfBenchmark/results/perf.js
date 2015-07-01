640a577569d5e05fb50797eaf48ef2afc6ac413d
Philipp Sieder <philipp.sieder@web.de>
2015-07-01 22:31:06 +0200

    Extend Reporting: Add value mouse hover, add summary, adjust layout and scaling of axes

--- perfBenchmark/results/perf.js
+++ perfBenchmark/results/perf.js
@@ -1,52 +1,53 @@
+/**
+ * 
+ */
 
+
+
+/* 
+ * 
+ */
 function render_graphs(results) {
+    $("#headline").text($("#headline").text() +' "' + results['name'] + '"');
+    
     $('.chart, .small-chart').map(function() {
-        plot($(this), results);
+        plot0($(this), results);
     });
     $('.summary').map(function() {
         summarise($(this), results);
     });
 }
 
+
+
+
+/* 
+ * 
+ */
 function summarise(div, results) {
     var scenario = div.attr('data-scenario');
-    var mode     = div.attr('data-mode');
+    var data = results[scenario];
+
     var dataYfirst = div.attr('data-y1');
+    if( dataYfirst && (data[dataYfirst] !=  undefined ) ){
+        div.append('&#216; ' + attr_or_default(div, 'data-y1-label', dataYfirst) + '<strong>' + Math.round(data[dataYfirst]) + div.attr('data-y-axis-unit') + '</strong><br>');
+    }
+
     var dataYsecond = div.attr('data-y2');
-    var unitDataYAxis = div.attr('data-y-axis');
-    var data     = results[scenario];
-
-    var rate;
-    if (mode == 'two') {
-        rate = Math.round(data[dataYsecond]);
-    }
-    else if (mode == 'one') {
-        rate = Math.round(data[dataYfirst]);
-    }
-    else {
-        rate = Math.round((data[dataYsecond] + data[dataYfirst]) / 2);
+    if( dataYsecond && (data[dataYsecond] != undefined ) ){
+        div.append('&#216; ' + attr_or_default(div, 'data-y2-label', dataYsecond) + '<strong>' + Math.round(data[dataYsecond]) + div.attr('data-y-axis-unit') + '</strong><br>');
     }
 
-    div.append('<strong>' + rate + '</strong>' + unitDataYAxis);
-}
-
-function plot(div, results) {
-    var file = div.attr('data-file');
-
-    if (file == undefined) {
-        plot0(div, results);
-    }
-    else {
-        $.ajax({
-            url: file,
-            success: function(data) {
-                plot0(div, JSON.parse(data));
-            },
-            fail: function() { alert('error loading ' + file); }
-        });
+    var dataYthird = div.attr('data-y3');
+    if( (div.attr('data-show-y3') == 'true') && dataYthird && (data[dataYthird] !=  undefined ) ){
+        div.append('&#216; ' + attr_or_default(div, 'data-y3-label', dataYthird) + '<strong>' + Math.round(data[dataYthird]) + div.attr('data-y-axis2-unit') + '</strong>');
     }
 }
 
+
+/* 
+ * 
+ */
 function plot0(div, results) {
     var type     = div.attr('data-type');
     var scenario = div.attr('data-scenario');
@@ -55,39 +56,30 @@ function plot0(div, results) {
         var data = results[scenario];
         plot_time(div, data);
     }
-    else {
-        var dimensions       = results[scenario]['dimensions'];
-        var dimension_values = results[scenario]['dimension-values'];
-        var data             = results[scenario]['data'];
-
-        if (type == 'series') {
-            plot_series(div, dimensions, dimension_values, data);
-        }
-        else if (type == 'x-y') {
-            plot_x_y(div, dimensions, dimension_values, data);
-        }
-        else if (type == 'r-l') {
-            plot_r_l(div, dimensions, dimension_values, data);
-        }
-    }
 }
 
+
+/* 
+ * 
+ */
 function plot_time(div, data) {
-    var show_y3 = div.attr('data-show-y3') == 'true';
     var dataYfirst = div.attr('data-y1');
     var dataYsecond = div.attr('data-y2');
-    var dataYthird = div.attr('data-y3');
     var labelYfirst = attr_or_default(div, 'data-y1-label', dataYfirst);
     var labelYsecond = attr_or_default(div, 'data-y2-label', dataYsecond);
-    var labelYthird = attr_or_default(div, 'data-y3-label', dataYthird);
+    var show_y3 = div.attr('data-show-y3') == 'true';
+    if (show_y3) {
+      var dataYthird = attr_or_default(div, 'data-y3','');
+      var labelYthird = attr_or_default(div, 'data-y3-label', dataYthird);
+    } 
     var chart_data = [];
-  
     var keys = show_y3
        ? [dataYfirst, dataYsecond, dataYthird]
         : [dataYfirst, dataYsecond];
     var labels = show_y3
        ? [labelYfirst, labelYsecond, labelYthird]
         : [labelYfirst, labelYsecond];
+
     $.each(keys, function(i, plot_key) {
         var d = [];
         $.each(data['samples'], function(j, sample) {
@@ -96,77 +88,28 @@ function plot_time(div, data) {
         var yaxis = (plot_key.indexOf(dataYthird) == -1 ? 1 : 2);
         chart_data.push({label: labels[i], data: d, yaxis: yaxis});
     });
+    
+    var prop_y_axes = [{ position:"left" },{ position:"right" }];
+    if( div.attr('data-y-axis-minVal') ){
+        prop_y_axes[0].min = div.attr('data-y-axis-minVal');
+    }
+    if( div.attr('data-y-axis-maxVal') ){
+        prop_y_axes[0].max = div.attr('data-y-axis-maxVal');
+    }
+    if( div.attr('data-y-axis2-minVal') ){
+        prop_y_axes[1].min = div.attr('data-y-axis2-minVal');
+    }
+    if( div.attr('data-y-axis2-maxVal') ){
+        prop_y_axes[1].max = div.attr('data-y-axis2-maxVal');
+    }
 
-    plot_data(div, chart_data, {yaxes: axes_rate_and_cpu_load});
+    plot_data(div, chart_data, {yaxes: prop_y_axes});
 }
 
-function plot_series(div, dimensions, dimension_values, data) {
-    var x_key         = div.attr('data-x-key');
-    var series_key    = div.attr('data-series-key');
-    var series_first  = dimensions[0] == series_key;
-    var series_values = dimension_values[series_key];
-    var x_values      = dimension_values[x_key];
-    var dataYfirst = div.attr('data-y1');
-    var dataYsecond = div.attr('data-y2');
-    var plot_key      = attr_or_default(div, 'plot-key', dataYsecond);
 
-    var chart_data = [];
-    $.each(series_values, function(i, s_val) {
-        var d = [];
-        $.each(x_values, function(j, x_val) {
-            var val = series_first ? data[s_val][x_val] :
-                                     data[x_val][s_val];
-            d.push([x_val, val[plot_key]]);
-        });
-        chart_data.push({label: series_key + ' = ' + s_val, data: d});
-    });
-
-    plot_data(div, chart_data);
-}
-
-function plot_x_y(div, dimensions, dimension_values, data) {
-    var x_key = div.attr('data-x-key');
-    var x_values = dimension_values[x_key];
-    var dataYfirst = div.attr('data-y1');
-    var dataYsecond = div.attr('data-y2');
-    var plot_keys = attr_or_default(div, 'plot-keys', dataYsecond).split(' ');
-    var chart_data = [];
-    var extra = {};
-    $.each(plot_keys, function(i, plot_key) {
-        var d = [];
-        $.each(x_values, function(j, x_val) {
-            d.push([x_val, data[x_val][plot_key]]);
-        });
-        var yaxis = 1;
-        if (plot_key.indexOf('bytes') != -1) {
-            yaxis = 2;
-            extra = {yaxes: axes_rate_and_bytes};
-        }
-        chart_data.push({label: plot_key, data: d, yaxis: yaxis});
-    });
-    plot_data(div, chart_data, extra);
-}
-
-function plot_r_l(div, dimensions, dimension_values, data) {
-    var x_values = dimension_values['producerRateLimit'];
-    var dataYfirst = div.attr('data-y1');
-    var dataYsecond = div.attr('data-y2');
-    var chart_data = [];
-    var d = [];
-    $.each(x_values, function(i, x_val) {
-        d.push([x_val, data[x_val][dataYsecond]]);
-    });
-    chart_data.push({label: 'rate achieved', data: d, yaxis: 1});
-
-    d = [];
-    $.each(x_values, function(i, x_val) {
-        d.push([x_val, data[x_val]['cpu-load']]);
-    });
-    chart_data.push({label: 'cpu-load (us)', data: d, yaxis: 2});
-
-    plot_data(div, chart_data, {yaxes: axes_rate_and_cpu_load});
-}
-
+/* 
+ * 
+ */
 function plot_data(div, chart_data, extra) {
     var legend     = attr_or_default(div, 'data-legend', 'se');
     var x_axis_log = attr_or_default(div, 'x-axis-log', 'false') == 'true';
@@ -174,9 +117,9 @@ function plot_data(div, chart_data, extra) {
 
     var chrome = {
         series: { lines: { show: true } },
-        grid:   { borderWidth: 2, borderColor: "#aaa" },
-        xaxis:  { tickColor: "#fff" },
-        yaxis:  { tickColor: "#eee" },
+        grid:   { borderWidth: 2, borderColor: "#aaa" , hoverable: true },
+        xaxis:  { tickColor: "#fff", labelHeight: 30 },
+        yaxis:  { tickColor: "#eee", labelWidth: 50 },
         legend: { position: legend, backgroundOpacity: 0.5 }
     };
 
@@ -199,14 +142,42 @@ function plot_data(div, chart_data, extra) {
     row.wrap('<table class="' + cssClass + '-wrapper"/>');
 
     cell.before('<td class="yaxis">' + div.attr('data-y-axis') + '</td>');
-    if (div.attr('data-y-axis2')) {
+    if ( div.attr('data-show-y3') == 'true' && div.attr('data-y-axis2') ) {
         cell.after('<td class="yaxis">' + div.attr('data-y-axis2') + '</td>');
     }
     row.after('<tr><td></td><td class="xaxis">' + div.attr('data-x-axis') +
               '</td><td></td></tr>');
-
+    registerToolTip(div);
     $.plot(div, chart_data, chrome);
 }
+
+
+/*
+ *
+ */
+function registerToolTip(div){
+    $("<div id='tooltip'></div>").css({
+        position: "absolute",
+        display: "none",
+        border: "1px solid #fdd",
+        padding: "2px",
+        "background-color": "#fee",
+        opacity: 0.80
+    }).appendTo("body");
+
+    div.bind("plothover", function (event, pos, item) {
+        if (item) {
+            var x = item.datapoint[0].toFixed(0);
+            var y = item.datapoint[1].toFixed(0);
+            $("#tooltip").html(item.series.label + " - Bei " + x + "s: " + y)
+                .css({top: item.pageY+5, left: item.pageX+5})
+                .fadeIn(200);
+        } else {
+            $("#tooltip").hide();
+        }
+   });
+}
+
 
 
 function log_transform(v) {
@@ -228,12 +199,27 @@ function attr_or_default(div, key, def) {
     return res == undefined ? def : res;
 }
 
-var axes_rate_and_cpu_load = [{ min:0, position:"left" },
-                              { min:0, position:"right" }];
-
-var axes_rate_and_bytes = [{ min:0},
-                           { min:0, position:"right" }];
-
 var log_x_axis = {min:1,
                   transform: log_transform,
                   ticks:     log_ticks};
+
+                  
+/*
+ * @unused
+ */
+function plot(div, results) {
+    var file = div.attr('data-file');
+
+    if (file == undefined) {
+        plot0(div, results);
+    }
+    else {
+        $.ajax({
+            url: file,
+            success: function(data) {
+                plot0(div, JSON.parse(data));
+            },
+            fail: function() { alert('error loading ' + file); }
+        });
+    }
+}
