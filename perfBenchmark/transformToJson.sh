79e8cc21ee83418e5d0c1d5197ee89e6bb32d4e1
Philipp Sieder <philipp.sieder@web.de>
2015-07-02 14:39:19 +0200

    Adjust Report Layout to save space, Extend Avg calculation

--- perfBenchmark/transformToJson.sh
+++ perfBenchmark/transformToJson.sh
@@ -17,8 +17,8 @@ numericbkp=$LC_NUMERIC
 export LC_NUMERIC="en_US.UTF-8"
 
 
-# Skip first line with column names and second line with measured value (falsified by start)  and extract the cpu, used RAM, net io(rx), net io(tx) and the free hdd space
-tail -n +3 "$fileToConvert" | awk -F "," '{print $110 "," $90 "," $27 "," $24 "," $83 "," NF }' > "$tmp"
+# Skip first line with column names and two lines with measured value (falsified by start)  and extract the cpu, used RAM, net io(rx), net io(tx) and the free hdd space
+tail -n +4 "$fileToConvert" | awk -F "," '{print $110 "," $90 "," $27 "," $24 "," $83 "," NF }' > "$tmp"
 
 
 # Start creating target json
@@ -30,15 +30,22 @@ entryprefix=""
 	# Save old separator and set new separator
 	OLDIFS=$IFS
 	IFS=","
+	
+	# Add missing time axis
 	time=0
+	
+	# Init for avg calculations
 	i=0
 	sumcpu=0.0
+	summem=0
+	sumhdd=0
 	sumnetrx=0
 	sumnettx=0
 
 	# Extract for each line
 	while read cpu memused netrx nettx hddfree count
 	 do
+	  # Workaround for buggy csv-export of glances - some lines are missing columns
 	  if [ "$count" -ne 112 ]; then
 	   let time+=5000
 	   continue
@@ -57,14 +64,16 @@ entryprefix=""
 	  printf '      "elapsed":%u,\n' "$time" >> "$target"
 	  printf '      "cpu-load":%03.1f,\n' "$cpu" >> "$target"
 	  printf '      "memused-MB":%u,\n' $memused >> "$target"
+	  printf '      "hddfree-MB":%u,\n' $hddfree >> "$target"
 	  printf '      "net-rate-rx-Kbps":%u,\n' $netrx >> "$target"
-	  printf '      "net-rate-tx-Kbps":%u,\n' $nettx >> "$target"
-	  printf '      "hddfree-MB":%u\n' $hddfree >> "$target"
+	  printf '      "net-rate-tx-Kbps":%u\n' $nettx >> "$target"
 	  printf '     }' >> "$target"
 	  
 	  # Adjust counter, sum up for avg calculation
 	  let time+=5000
 	  sumcpu=`bc <<< "scale=1; $sumcpu + $cpu"`
+	  let summem+=$memused
+	  let sumhdd+=$hddfree
 	  let sumnetrx+=$netrx
 	  let sumnettx+=$nettx
 	  let i++
@@ -77,6 +86,8 @@ entryprefix=""
 # Calc and append average values to json
 printf '],\n' >> "$target"
 printf '    "avg-cpu-load":%02.1f,\n' `bc <<< "scale=1; $sumcpu/$i"` >> "$target"
+printf '    "avg-memused-MB":%02.1f,\n' `bc <<< "scale=1; $summem/$i"` >> "$target"
+printf '    "avg-hddfree-MB":%02.1f,\n' `bc <<< "scale=1; $sumhdd/$i"` >> "$target"
 printf '    "avg-net-rate-rx-Kbps":%02.1f,\n' `bc <<< "scale=1; $sumnetrx/$i"` >> "$target"
 printf '    "avg-net-rate-tx-Kbps":%02.1f\n' `bc <<< "scale=1; $sumnettx/$i"` >> "$target"
 echo -e '   },' >> "$target"
